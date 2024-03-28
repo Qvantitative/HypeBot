201db6d40b43598d53e6fb15bfd74f8639665379
qvantitative <qvantitative@gmail.com>
2024-03-28 02:32:58 -0400

    queue

--- commands/queue.js
+++ commands/queue.js
@@ -1,38 +1,42 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 const { useQueue } = require('discord-player');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("queue")
-        .setDescription("shows first 10 songs in the queue"),
+        .setDescription("Shows first 10 songs in the queue"),
 
     execute: async ({ interaction }) => {
-        const queue = useQueue(interaction.guild.Id)
+        const queue = useQueue(interaction.guild.id);
 
-        // Check if the queue exists and if it's not empty
-        if (!queue || queue.tracks.size === 0) {
-            // If the queue doesn't exist or it's empty, reply with an error message
+        if (!queue) {
+            return interaction.reply({content: "I am not in a voice channel", ephemeral: true});
+        }
+
+        if (!queue.tracks || queue.tracks.size === 0) {
             return interaction.reply({ content: "There is no queue to display.", ephemeral: true });
         }
 
-        // Get the first 10 songs in the queue
-        const queueString = queue.tracks.map((song, i) => {
-            return `${i}) [${song.duration}]\` ${song.title} - <@${song.requestedBy.id}>`
-        }).join("\n")
+        let pagesNum = Math.ceil(queue.tracks.size / 5);
+        if (pagesNum <= 0) pagesNum = 1;
 
-        // Get the current song
-        const currentSong = queue.currentTrack
+        const tracks = queue.tracks.map((track, idx) => `**${++idx})** [${track.title}](${track.url})`);
+        const paginatedMessages = [];
 
-        await interaction.reply({
-            embeds: [
-                new MessageEmbed()
-                    .setDescription(`**Currently Playing**\n` + 
-                        (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} - <@${currentSong.requestedBy.id}>` : "None") +
-                        `\n\n**Queue**\n${queueString}`
-                    )
-                    .setThumbnail(currentSong.thumbnail)
-            ]
-        })
+        for (let i = 0; i < pagesNum; i++) {
+            const list = tracks.slice(i * 5, i * 5 + 5).join('\n');
+
+            const embed = new MessageEmbed()
+                .setColor('Red')
+                .setDescription(`**Queue** for **session** in **${queue.channel?.name}:**\n${list === '' ? '\n*• No more queued tracks*' : `\n${list}`}\n**Now Playing:** [${queue.currentTrack?.title}](${queue.currentTrack?.url})\n`)
+                .setFooter({
+                    text: `${queue.tracks.size} track(s) in queue`
+                });
+
+            paginatedMessages.push(embed);
+        }
+
+        return interaction.reply({ embeds: paginatedMessages });
     }
-}
+};
