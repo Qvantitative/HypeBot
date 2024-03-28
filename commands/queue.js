81bd28291887c53ea56a67c5d7d39a97dece3a8a
qvantitative <qvantitative@gmail.com>
2024-03-28 02:50:35 -0400

    queue

--- commands/queue.js
+++ commands/queue.js
@@ -10,31 +10,26 @@ module.exports = {
     execute: async ({ interaction }) => {
         const queue = useQueue(interaction.guild.id);
 
-        if (!queue) {
-            return interaction.reply({content: "I am not in a voice channel", ephemeral: true});
-        }
-
-        if (!queue.tracks || queue.tracks.size === 0) {
+        if (!queue || queue.tracks.size === 0) {
             return interaction.reply({ content: "There is no queue to display.", ephemeral: true });
         }
 
-        let pagesNum = Math.ceil(queue.tracks.size / 5);
-        if (pagesNum <= 0) pagesNum = 1;
+        const queueString = queue.tracks.map((song, i) => {
+            return `${i}) [${song.duration}]\` ${song.title} - <@${song.requestedBy.id}>`;
+        }).join("\n");
 
-        const tracks = queue.tracks.map((track, idx) => `**${++idx})** [${track.title}](${track.url})`);
-        const paginatedMessages = [];
+        const currentSong = queue.currentTrack;
 
-        for (let i = 0; i < pagesNum; i++) {
-            const list = tracks.slice(i * 5, i * 5 + 5).join('\n');
+        const messageContent = `**Currently Playing**\n` +
+            (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} - <@${currentSong.requestedBy.id}>` : "None") +
+            `\n\n**Queue**\n${queueString}`;
 
-            const embed = new MessageEmbed()
-                .setColor('Red')
-                .setDescription(`**Queue** for **session** in **${queue.channel?.name}:**\n${list === '' ? '\n*• No more queued tracks*' : `\n${list}`}\n**Now Playing:** [${queue.currentTrack?.title}](${queue.currentTrack?.url})\n`)
-                .setFooter(`${queue.tracks.size} track(s) in queue`);
-
-            paginatedMessages.push(embed);
-        }
-
-        return interaction.reply({ embeds: paginatedMessages });
+        return interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription(messageContent)
+                    .setThumbnail(currentSong.thumbnail)
+            ]
+        });
     }
 };
