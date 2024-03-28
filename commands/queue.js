eb91a30e49b8b9a5e8c3bbf9c2af882e3e107905
qvantitative <qvantitative@gmail.com>
2024-03-28 02:58:20 -0400

    queue

--- commands/queue.js
+++ commands/queue.js
@@ -10,13 +10,16 @@ module.exports = {
     execute: async ({ interaction }) => {
         const queue = useQueue(interaction.guild.id);
 
-        if (!queue || queue.tracks.size === 0) {
+        if (!queue || !queue.tracks || queue.tracks.size === 0) {
             return interaction.reply({ content: "There is no queue to display.", ephemeral: true });
         }
 
-        const queueString = queue.tracks.map((song, i) => {
-            return `${i}) [${song.duration}]\` ${song.title} - <@${song.requestedBy.id}>`;
-        }).join("\n");
+        const queueString = queue.tracks
+            .filter(song => song && song.requestedBy && song.requestedBy.id)
+            .map((song, i) => {
+                return `${i}) [${song.duration}]\` ${song.title} - <@${song.requestedBy.id}>`;
+            })
+            .join("\n");
 
         const currentSong = queue.currentTrack;
 
@@ -24,12 +27,10 @@ module.exports = {
             (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} - <@${currentSong.requestedBy.id}>` : "None") +
             `\n\n**Queue**\n${queueString}`;
 
-        return interaction.reply({
-            embeds: [
-                new MessageEmbed()
-                    .setDescription(messageContent)
-                    .setThumbnail(currentSong.thumbnail)
-            ]
-        });
+        const embed = new MessageEmbed()
+            .setDescription(messageContent)
+            .setThumbnail(currentSong ? currentSong.thumbnail : '');
+
+        return interaction.reply({ embeds: [embed] });
     }
 };
